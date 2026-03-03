c212343da4386916db3432eb0c59c742a09eb231
Kriggle <91673050+ItzNugget5@users.noreply.github.com>
2026-03-02 20:28:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.addEventListener("mousemove", function(e) {
+    console.log("Mouse X:", e.clientX);
+});
+
+document.body.addEventListener("click", function() {
+    document.body.style.backgroundColor = "black";
+});
+
 document.getElementById("btn").addEventListener("click", function() {
     alert("You clicked the button!");
+
 });
